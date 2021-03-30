118075cef0a19f3d7d6fece31cd23624e8102744
LautaroCoccia <lautaroauditore@gmail.com>
2021-03-30 13:41:54 -0300

    Textures added

---
+++ Practicas profesionales 2/Assets/Scripts/Planet.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public Transform target;
+    Vector3 position;
+
+    double sinA;
+    double cosA;
+
+    float distance;
+    float myDeltaTime;
+    
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position.x;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myDeltaTime += Time.deltaTime * speed;
+
+        position = new Vector3(distance * Mathf.Cos(myDeltaTime), transform.position.y, distance * Mathf.Sin(myDeltaTime));
+
+        transform.position = position;
+
+        transform.position += target.position;
+        //rota sobre su propio eje
+        //transform.rotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up);
+    }
+}
