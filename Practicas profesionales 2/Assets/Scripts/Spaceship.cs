a696af1c36ddec0627e0432012dcc39121fde57d
LautaroCoccia <lautaroauditore@gmail.com>
2021-03-31 16:06:11 -0300

    Spaceship script & tag added

---
+++ Practicas profesionales 2/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public float speed = 0;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 direct = new Vector3(hor, 0, ver);
+
+        transform.position += direct * speed * Time.deltaTime;
+
+    }
+}
