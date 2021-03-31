911e88177252c1e3afd10fd042c88fe90c6fe447
LautaroCoccia <lautaroauditore@gmail.com>
2021-03-31 16:04:41 -0300

    Updated planet script

--- Practicas profesionales 2/Assets/Scripts/Planet.cs
+++ Practicas profesionales 2/Assets/Scripts/Planet.cs
@@ -3,34 +3,48 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public Transform target;
-    Vector3 position;
+    //public Transform target;
+    //Vector3 position;
 
-    double sinA;
-    double cosA;
+    //double sinA;
+    //double cosA;
 
-    float distance;
-    float myDeltaTime;
-    
-    public float speed;
-
+    //float distance;
+    //float myDeltaTime;
+    public float radius;
+    public float speed = 1 ;
+    public float angle = 50;
+    public float rotationangle = 100;
     // Start is called before the first frame update
     void Start()
     {
-        distance = transform.position.x;
+        radius = transform.position.x;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        myDeltaTime += Time.deltaTime * speed;
+        Vector3 v3 = Vector3.zero;
+        angle += speed * Time.deltaTime;
+        rotationangle += speed * Time.deltaTime;
 
-        position = new Vector3(distance * Mathf.Cos(myDeltaTime), transform.position.y, distance * Mathf.Sin(myDeltaTime));
+        v3.x = radius * Mathf.Cos(angle);
+        v3.z = radius * Mathf.Sin(angle);
 
-        transform.position = position;
+        transform.position = v3;
+        transform.rotation = Quaternion.Euler(0, rotationangle, 0);
+    }
 
-        transform.position += target.position;
+    void MyPlanetmovement()
+    {
+        //myDeltaTime += Time.deltaTime * speed;
+
+        //position = new Vector3(distance * Mathf.Cos(myDeltaTime), transform.position.y, distance * Mathf.Sin(myDeltaTime));
+
+        //transform.position = position;
+
+        //transform.position += target.position;
         //rota sobre su propio eje
         //transform.rotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up);
     }
