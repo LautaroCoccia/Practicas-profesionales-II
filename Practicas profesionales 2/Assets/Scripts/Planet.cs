b4a7bd24ac666d44e1c0290420e4296d9503e7ad
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-03 00:35:09 -0300

    Planet.cs Updated

--- Practicas profesionales 2/Assets/Scripts/Planet.cs
+++ Practicas profesionales 2/Assets/Scripts/Planet.cs
@@ -3,27 +3,24 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    //public Transform target;
-    //Vector3 position;
-
-    //double sinA;
-    //double cosA;
-
-    //float distance;
-    //float myDeltaTime;
     public float radius;
     public float speed = 1 ;
     public float angle = 50;
     public float rotationangle = 100;
+
     // Start is called before the first frame update
     void Start()
     {
         radius = transform.position.x;
         
     }
-
     // Update is called once per frame
     void Update()
+    {
+        Planetmovement();
+    }
+
+    void Planetmovement()
     {
         Vector3 v3 = Vector3.zero;
         angle += speed * Time.deltaTime;
@@ -34,18 +31,5 @@ public class Planet : MonoBehaviour
 
         transform.position = v3;
         transform.rotation = Quaternion.Euler(0, rotationangle, 0);
-    }
-
-    void MyPlanetmovement()
-    {
-        //myDeltaTime += Time.deltaTime * speed;
-
-        //position = new Vector3(distance * Mathf.Cos(myDeltaTime), transform.position.y, distance * Mathf.Sin(myDeltaTime));
-
-        //transform.position = position;
-
-        //transform.position += target.position;
-        //rota sobre su propio eje
-        //transform.rotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up);
     }
 }
