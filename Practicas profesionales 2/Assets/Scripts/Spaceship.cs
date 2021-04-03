34facf309e518e0e3e81ca39fd5e5239f9d364cf
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-03 00:36:29 -0300

    Spaceship movement updated

--- Practicas profesionales 2/Assets/Scripts/Spaceship.cs
+++ Practicas profesionales 2/Assets/Scripts/Spaceship.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
-    public float speed = 0;
-    // Start is called before the first frame update
-    
+    public float maxSpeed = 0;
+    public float rotationSpeed = 0;
 
+   
     // Update is called once per frame
     void Update()
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        Vector3 direct = new Vector3(hor, 0, ver);
+        Vector3 direct = new Vector3(hor, 0, 0);
 
-        transform.position += direct * speed * Time.deltaTime;
+        //transform.position += direct * speed * Time.deltaTime;
+        if(ver>0)
+        {
+            transform.Translate(Vector3.forward * maxSpeed * Time.deltaTime);
+        }
+        else if(ver < 0)
+        {
+            transform.Translate(Vector3.back * maxSpeed * Time.deltaTime);
+        }
 
+        if(hor>0)
+        {
+            transform.Rotate(new Vector3(0, hor * rotationSpeed * Time.deltaTime, 0));
+        }
+        else if (hor < 0)
+        {
+            transform.Rotate(new Vector3(0, hor * rotationSpeed * Time.deltaTime, 0));
+        }
     }
 }
