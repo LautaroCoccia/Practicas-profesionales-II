9ffd17fe70b7cca902440098f7fc07e75f6f9ad8
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-06 18:04:58 -0300

    Sun script added

---
+++ Practicas profesionales 2/Assets/Scripts/Sun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    [SerializeField] private Light myLight;
+    [SerializeField] private float myIntensity = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLight.intensity = Mathf.Sin(myIntensity);
+    }
+}
