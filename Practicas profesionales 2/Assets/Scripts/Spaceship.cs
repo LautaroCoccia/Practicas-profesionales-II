15aa4cd11d428b9d4c94e954de1eac0377d80989
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-06 18:10:25 -0300

    Update Spaceship.cs

--- Practicas profesionales 2/Assets/Scripts/Spaceship.cs
+++ Practicas profesionales 2/Assets/Scripts/Spaceship.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
-    public float maxSpeed = 0;
-    public float rotationSpeed = 0;
+    [SerializeField] private float maxSpeed = 0;
+    [SerializeField] private float rotationSpeed = 0;
+    [SerializeField] private float maxTime;
+    [SerializeField] private float minTime= 0.075f;
+    TrailRenderer spaceshipTrail;
 
-   
+    private void Start()
+    {
+        spaceshipTrail = GetComponent<TrailRenderer>();
+        maxTime = spaceshipTrail.time;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,17 +20,16 @@ public class Spaceship : MonoBehaviour
         float ver = Input.GetAxis("Vertical");
 
         Vector3 direct = new Vector3(hor, 0, 0);
-
-        //transform.position += direct * speed * Time.deltaTime;
-        if(ver>0)
+        if (ver>0)
         {
+            spaceshipTrail.time = maxTime;
             transform.Translate(Vector3.forward * maxSpeed * Time.deltaTime);
         }
         else if(ver < 0)
         {
+            spaceshipTrail.time = minTime;
             transform.Translate(Vector3.back * maxSpeed * Time.deltaTime);
         }
-
         if(hor>0)
         {
             transform.Rotate(new Vector3(0, hor * rotationSpeed * Time.deltaTime, 0));
