bac6ce2164746fd722f7bf6f8b31e0704032e5cc
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-07 00:26:39 -0300

    Fading planets

--- Practicas profesionales 2/Assets/Scripts/Planet.cs
+++ Practicas profesionales 2/Assets/Scripts/Planet.cs
@@ -3,23 +3,36 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer meshPlanet;
+    [SerializeField] private Transform spaceship;
     [SerializeField] private float radius;
     [SerializeField] private float translationSpeed = 1;
     [SerializeField] private float translationRadius = 50;
     [SerializeField] private float rotationSpeed = 1;
     [SerializeField] private Vector3 rotationAxis = new Vector3(0, 100,0);
+    Material mat;
+    Color originalColor;
     Vector3 v3 = Vector3.zero;
-    Transform spaceshipObj;
     // Start is called before the first frame update
     void Start()
     {
-       
+        
+        mat = meshPlanet.material;
+        originalColor = mat.color;
         radius = transform.position.x;
     }
     // Update is called once per frame
     void Update()
     {
         Planetmovement();
+        if(CalculateDistance()< transform.localScale.x)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 0.5f);
+        }
+        else
+        {
+            mat.color = originalColor;
+        }
     }
 
     void Planetmovement()
@@ -33,6 +46,10 @@ public class Planet : MonoBehaviour
         transform.position = v3;
 
         transform.rotation = Quaternion.Euler(rotationAxis);
+    }
+    float CalculateDistance()
+    {
+        return Vector3.Distance(transform.position, spaceship.position);
     }
 }
 
